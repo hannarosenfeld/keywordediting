98c736edcdf63ef9bcde6df80e7de2db10e6e4e2
Thomas Schreiber <schreibert@email.chop.edu>
2019-07-24 21:57:34 -0400

    Improve contact form for netlify.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { navigateTo } from 'gatsby-link'
 import { FormattedMessage } from 'gatsby-plugin-intl'
 
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 export default class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
   render() {
     let close = (
       <div
@@ -24,23 +55,45 @@ export default class Contact extends React.Component {
         </h2>
         <form
           name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-honeypot="last_name"
+          method="post"
           action="/success/"
-          enctype="application/x-www-form-urlencoded"
+          data-netlify="true"
+          data-netlify-honeypot="last-name"
         >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{' '}
+              <input name="last-name" onChange={this.handleChange} />
+            </label>
+          </p>
           <div className="field half first">
             <label htmlFor="name">Name</label>
-            <input name="name" id="name" type="text" />
+            <input
+              name="name"
+              id="name"
+              type="text"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="field half">
             <label htmlFor="email">Email</label>
-            <input name="email" id="email" type="email" />
+            <input
+              name="email"
+              id="email"
+              type="email"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="4" />
+            <textarea
+              name="message"
+              id="message"
+              rows="4"
+              onChange={this.handleChange}
+            />
           </div>
           <ul className="actions">
             <li>
