58a8c9008360f9f6c734ec5b9549363da9817340
Hanna Rosenfeld <thesoftestbrain@gmail.com>
2019-07-24 21:23:50 -0400

    Closes #14: Style 404 Page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withIntl, FormattedMessage } from 'gatsby-plugin-intl'
+import { withIntl, FormattedHTMLMessage } from 'gatsby-plugin-intl'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -8,10 +8,10 @@ const NotFoundPage = ({ intl }) => (
   <Layout>
     <SEO title={`404: ${intl.formatMessage({ id: 'title' })}`} />
     <h1>
-      <FormattedMessage id="notfound" />
+      <FormattedHTMLMessage id="notfound" />
     </h1>
     <p>
-      <FormattedMessage id="notfound_p" />
+      <FormattedHTMLMessage id="notfound_p" />
     </p>
   </Layout>
 )
