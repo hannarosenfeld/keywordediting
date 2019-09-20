81e00c9c8d4be7aff3a142e828e51f6a1c1eab78
Thomas Schreiber <schreibert@email.chop.edu>
2019-09-20 01:27:02 -0400

    Remove unused import.

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { injectIntl, FormattedHTMLMessage } from 'gatsby-plugin-intl'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const SuccessPage = ({ intl }) => (
   <Layout>
